078a516b3c16884c4f9f7fa5987d91f0ea718466
Thomas Laux <laux.thomas@t-online.de>
2019-05-06 16:16:59 +0200

    weird stuff iz happening!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,9 +19,11 @@ import static org.lwjgl.opengl.GL15.glBindBuffer;
 import static org.lwjgl.opengl.GL15.glBufferData;
 import static org.lwjgl.opengl.GL15.glClear;
 import static org.lwjgl.opengl.GL15.glClearColor;
-import static org.lwjgl.opengl.GL15.glDrawArrays;
 import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL20.GL_ELEMENT_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL20.GL_FLOAT;
 import static org.lwjgl.opengl.GL20.GL_TRUE;
+import static org.lwjgl.opengl.GL20.glDrawArrays;
 import static org.lwjgl.opengl.GL30.glBindFragDataLocation;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
@@ -43,6 +45,7 @@ public class Main {
             if (glfwWindowShouldClose(window)) {
                 running = false;
                 glfwTerminate();
+                break;
 
             }
         }
@@ -51,6 +54,10 @@ public class Main {
 
     private void update() {
         glfwPollEvents();
+//        calculatePulseColor();
+    }
+
+    private void calculatePulseColor() {
         if (pulseUp) {
             pulseColor += 0.01f;
             if (pulseColor >= 1.0f) {
@@ -67,7 +74,9 @@ public class Main {
     private void render() {
         glUniform3f(uniColor, pulseColor, 0.0f, 0.0f);
         // Draw a triangle from the 3 vertices
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        //use only 4 vertices instead of 6:
+//        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
 
@@ -100,25 +109,50 @@ public class Main {
         }
         initializeWindow();
 
+
+        float[] vertices = {
+                //x,y,r,g,b,s,t
+                -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Top-left
+                0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,// Top-right
+                0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, // Bottom-right
+
+                0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f,// Bottom-right
+                -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f // Bottom-left
+                - 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f  // Top-left
+        };
+        //<editor-fold desc="VAO Stuff">
         //Creating a VertexArrayObject
         int vao = GL30.glGenVertexArrays();
         GL30.glBindVertexArray(vao);
 
         // Create a Vertex Buffer Object and copy the vertex data to it
         int vbo = GL15.glGenBuffers();
-        float[] vertices = {
-                0.0f, 0.5f, 1.0f, 0.0f, 0.0f, // Vertex 1: Red
-                0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // Vertex 2: Green
-                -0.5f, -0.5f, 0.0f, 0.0f, 1.0f  // Vertex 3: Blue
-        };
         //Create vertex buffer
         FloatBuffer verticeBuffer = BufferUtils.createFloatBuffer(vertices.length);
         verticeBuffer.put(vertices).flip();
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
         //Send vertice buffer to VBO
         glBufferData(GL_ARRAY_BUFFER, verticeBuffer, GL_STATIC_DRAW);
+        //</editor-fold>
 
 
+        //<editor-fold desc="EBO stuff">
+        //Creating a ElementBufferObject
+        int ebo = GL30.glGenBuffers();
+        //Create Element Buffer Object:
+        float[] elements = {
+                0, 1, 2,
+                2, 3, 0
+        };
+        FloatBuffer elementBuffer = BufferUtils.createFloatBuffer(elements.length);
+        elementBuffer.put(elements).flip();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        //Send buffer to ebo
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_STATIC_DRAW);
+        //</editor-fold>
+
+
+        //<editor-fold desc="Shader Stuff">
         //Create Shaders:
         String fragmentSource = createFragmentShader();
         String vertexSource = createVertexShader();
@@ -143,30 +177,53 @@ public class Main {
         glBindFragDataLocation(shaderProgram, 0, "outColor");
         glLinkProgram(shaderProgram);
         glUseProgram(shaderProgram);
+        //</editor-fold>
 
-        /*
-        // Specify the layout of the vertex data POSITION
+
+        //<editor-fold desc="Attrib Pointer Stuff">
         int posAttrib = glGetAttribLocation(shaderProgram, "position");
-        //how the data for that input is retrieved from the array:
         glEnableVertexAttribArray(posAttrib);
-        glVertexAttribPointer(posAttrib, 2, GL_FLOAT, false, 5, 0);
+        glVertexAttribPointer(posAttrib, 2, GL_FLOAT, false, 8 * sizeOfFloat, 0);
 
-        // Specify the layout of the vertex data COLOR
         int colAttrib = glGetAttribLocation(shaderProgram, "color");
-        //how the data for that input is retrieved from the array:
         glEnableVertexAttribArray(colAttrib);
-        glVertexAttribPointer(colAttrib, 3, GL_FLOAT, false, 5, 2);
+        glVertexAttribPointer(colAttrib, 3, GL_FLOAT, false, 8 * sizeOfFloat, 2 * sizeOfFloat);
+
+        int texAttrib = glGetAttribLocation(shaderProgram, "color");
+        glEnableVertexAttribArray(texAttrib);
+        glVertexAttribPointer(texAttrib, 2, GL_FLOAT, false, 8 * sizeOfFloat, 5 * sizeOfFloat);
+        //</editor-fold>
+
+
+        //<editor-fold desc="Texture Stuff">
+        /*
+        int tex = glGenTextures();
+        glBindTexture(GL_TEXTURE_2D,tex);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        float[] color = { 1.0f, 0.0f, 0.0f, 1.0f };
+        glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, color);
+
+        glGenerateMipmap(GL_TEXTURE_2D);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+
+        // Black/white checkerboard
+        float[] pixels = {
+                0.0f, 0.0f, 0.0f,   1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f
+        };
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_FLOAT, pixels);
         */
 
-        int posAttrib = glGetAttribLocation(shaderProgram, "position");
-        glEnableVertexAttribArray(posAttrib);
-        glVertexAttribPointer(posAttrib, 2, GL_FLOAT, false, 5*sizeOfFloat, 0);
 
-        int colAttrib = glGetAttribLocation(shaderProgram, "color");
-        glEnableVertexAttribArray(colAttrib);
-        glVertexAttribPointer(colAttrib, 3, GL_FLOAT, false, 5*sizeOfFloat, 2*sizeOfFloat);
+        //</editor-fold>
 
 
+        //<editor-fold desc="newTexuteres">
+//        Texture tex = new Texture("tree_bark_texture.png");
+//        tex.bind();
+        //</editor-fold>
         uniColor = glGetUniformLocation(shaderProgram, "triangleColor");
         glUniform3f(uniColor, 1.0f, 0.0f, 0.0f);
 
---
+++ src/main/java/Texture.java
@@ -0,0 +1,40 @@
+import org.lwjgl.BufferUtils;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.stb.STBImage.stbi_image_free;
+import static org.lwjgl.stb.STBImage.stbi_load;
+
+public class Texture {
+    private int width;
+    private int height;
+    private int id;
+
+    public Texture(String filename) {
+        IntBuffer width = BufferUtils.createIntBuffer(1);
+        IntBuffer height = BufferUtils.createIntBuffer(1);
+        IntBuffer comp = BufferUtils.createIntBuffer(1);
+
+        ByteBuffer data = stbi_load("./res/" + filename, width, height, comp, 4);
+
+        id = glGenTextures();
+        this.width = width.get();
+        this.height = height.get();
+
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this.width, this.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        stbi_image_free(data);
+    }
+
+    public void bind() {
+        glBindTexture(GL_TEXTURE_2D, id);
+    }
+
+    public void unbind() {
+        glBindTexture(GL_TEXTURE_2D, 0);
+    }
+}
